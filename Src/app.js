82174bca4680c51a54e4fd487e1bac48ae98c266
riishaluo <riishaluo@gmail.com>
2025-10-28 10:57:27 +0530

    Updated app.js to fix server issue

--- Src/app.js
+++ Src/app.js
@@ -7,11 +7,9 @@ const app = express()
 
 connectDB()
 
-const PORT = process.env.PORt || 5000 
+const PORT = process.env.PORT || 5000 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`);
-    
-
 })
 
 
