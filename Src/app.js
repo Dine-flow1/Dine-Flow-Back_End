f3d4a9959c01619956628f31f0b6d793dfc645f3
shibly5365 <nuhm8565@gmail.com>
2025-11-01 15:20:22 +0530

    updated small thigs

--- Src/app.js
+++ Src/app.js
@@ -1,19 +1,24 @@
 import express from "express";
 import connectDB from "./config/ConnectDb.js";
 import dotenv from "dotenv"; 
-
+import cookieParser from "cookie-parser"
 import Restaurantrouter from "./modules/Restaurant/routing/restaurantRoutes.js";
 import authRouting from "./modules/Auth/routing/authrouting.js";
 import ownerRoutes from "./modules/saasOwner/routing/saasRouting.js";
+import menuRoutin from "./modules/Menu/routing/menuRouting.js";
+import { authMiddleware } from "./middleware/authmiddleware.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouting);
 app.use("/api/restaurants", Restaurantrouter);
 app.use("/api/owner", ownerRoutes);
+app.use("/api/menu", menuRoutin);
+
 
 
 
