4144e298d26bf69ed661e4a8b43c0b64190fe531
shibly5365 <nuhm8565@gmail.com>
2025-11-06 09:59:29 +0530

    updated for orders

--- Src/app.js
+++ Src/app.js
@@ -6,6 +6,7 @@ import Restaurantrouter from "./modules/Restaurant/routing/restaurantRoutes.js";
 import authRouting from "./modules/Auth/routing/authrouting.js";
 import ownerRoutes from "./modules/saasOwner/routing/saasRouting.js";
 import menuRoutin from "./modules/Menu/routing/menuRouting.js";
+import orderRouting from "./modules/Orders/routing/orderRouting.js";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use("/api/auth", authRouting);
 app.use("/api/restaurants", Restaurantrouter);
 app.use("/api/owner", ownerRoutes);
 app.use("/api/menu", menuRoutin);
+app.use("/api/order", orderRouting);
 
 
 
