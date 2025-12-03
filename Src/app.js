25502eb59cef750547baf93f3a9c7f6be2fa1a83
shibly5365 <nuhm8565@gmail.com>
2025-12-03 16:38:18 +0530

    add updated app js

--- Src/app.js
+++ Src/app.js
@@ -1,18 +1,25 @@
 import express from "express";
 import connectDB from "./config/ConnectDb.js";
-import dotenv from "dotenv"; 
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import Restaurantrouter from "./modules/Restaurant/routing/restaurantRoutes.js";
 import authRouting from "./modules/Auth/routing/authrouting.js";
 import ownerRoutes from "./modules/saasOwner/routing/saasRouting.js";
-import paymentRoutes from "./modules/payment/routing/paymentRoutes.js"
+import paymentRoutes from "./modules/payment/routing/paymentRoutes.js";
 import menuRoutin from "./modules/Menu/routing/menuRouting.js";
 import orderRouting from "./modules/Orders/routing/orderRouting.js";
 import TableRouting from "./modules/Booking/Routing/tableRouting.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
@@ -25,13 +32,9 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/order", orderRouting);
 app.use("/api/tableBooking", TableRouting);
 
-
-
-
-
 connectDB();
 
 const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
